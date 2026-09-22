1db4850b443201ba1da57c3d85699ea03a1d5e2e
Ethan Chen <Ethanchen4@gmail.com>
2026-09-22 17:45:23 -0400

    Adding conditional styling to nav links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,8 @@ import { NavLink } from "react-router";
 import { FaSkull } from "react-icons/fa";
 
 const Navbar = () => {
+  const baseStyles = "transition hover:text-blue-400";
+  const activeStyles = "text-blue-400 font-semibold";
   return (
     <nav className="bg-gray-800 border-b border-gray-700 shadow-md sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -15,11 +17,46 @@ const Navbar = () => {
         {/* Desktop Navigation */}
         <div className="hidden md:flex items-center gap-6">
           <div className="space-x-4 text-sm text-gray-300">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/heroes">Heroes</NavLink>
-            <NavLink to="/blog">Blog</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? activeStyles : baseStyles
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? activeStyles : baseStyles
+              }
+              to="/heroes"
+            >
+              Heroes
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? activeStyles : baseStyles
+              }
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? activeStyles : baseStyles
+              }
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? activeStyles : baseStyles
+              }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </div>
         </div>
       </div>
