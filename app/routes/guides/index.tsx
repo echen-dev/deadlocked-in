d8ca373a90262364a47ea23d75f32b925b58dbb4
Ethan Chen <Ethanchen4@gmail.com>
2026-09-22 19:35:32 -0400

    Adding home and main layouts

--- app/routes/guides/index.tsx
+++ app/routes/guides/index.tsx
@@ -1,8 +1,8 @@
 const GuidesPage = () => {
   return (
-    <section>
+    <>
       <h2 className="text-3xl font-bold text-white mb-8">Guides</h2>
-    </section>
+    </>
   );
 };
 
--- app/routes/heroes/index.tsx
+++ app/routes/heroes/index.tsx
@@ -1,8 +1,8 @@
 const HeroesPage = () => {
   return (
-    <section>
+    <>
       <h2 className="text-3xl text-white font-bold mb-8">Heroes</h2>
-    </section>
+    </>
   );
 };
 
