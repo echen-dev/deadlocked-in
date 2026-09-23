669d1926a2b92240cd9030e19af50ecd37113d39
Ethan Chen <Ethanchen4@gmail.com>
2026-09-23 16:51:32 -0400

    Adding data to hero details page

--- app/routes/heroes/details.tsx
+++ app/routes/heroes/details.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/details";
 import type { Hero } from "~/types";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router";
 
 export async function clientLoader({
   request,
@@ -17,8 +19,40 @@ export function HydrateFallback() {
 
 const HeroDetailsPage = ({ loaderData }: Route.ComponentProps) => {
   const hero = loaderData;
-  console.log(hero);
-  return <>Hero Details Page</>;
+  return (
+    <>
+      <Link
+        to="/heroes"
+        className="flex items-center text-blue-400 hover:text-blue-500 mb-6 transition"
+      >
+        <FaArrowLeft className="mr-2" /> Back to Heroes
+      </Link>
+      <div className="grid gap-8 md:grid-cols-2 items-start">
+        <div>
+          <img
+            src={hero.image}
+            alt={hero.name}
+            className="w-full max-h-80 rounded-lg shadow-md object-cover"
+          />
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold text-blue-400 mb-4">{hero.name}</h1>
+          <p className="text-sm text-gray-300 mb-4">
+            Release Date: {new Date(hero.releaseDate).toLocaleDateString()}
+          </p>
+          <p className="text-gray-200 mb-6">{hero.description}</p>
+          <Link
+            to={"/guides"}
+            className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition"
+          >
+            <span className="flex items-center gap-2">
+              View {hero.name} Guides <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HeroDetailsPage;
